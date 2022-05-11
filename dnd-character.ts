82057f151244994ae927bf81d0b9130e903ac6e2
Shawn Cannon <scanno16@ford.com>
2022-05-11 10:28:54 -0600

    pass second test

--- dnd-character.ts
+++ dnd-character.ts
@@ -1,1 +1,45 @@
-export class DnDCharacter {}
+export class DnDCharacter {
+    strength: number;
+    dexterity: number;
+    constitution: number;
+    intelligence: number;
+    wisdom: number;
+    charisma: number;
+    hitpoints: number;
+
+
+
+    constructor() {
+
+        this.strength = DnDCharacter.generateAbilityScore();
+        this.dexterity = DnDCharacter.generateAbilityScore();
+        this.constitution = DnDCharacter.generateAbilityScore();
+        this.intelligence = DnDCharacter.generateAbilityScore();;
+        this.wisdom = DnDCharacter.generateAbilityScore();
+        this.charisma = DnDCharacter.generateAbilityScore();
+
+    }
+
+    static numberGenerator() {
+        return Math.ceil(Math.random() * 6)
+    }
+
+    static topthreeRolls() {
+        let diceRoll: number[] = []
+        while (diceRoll.length < 4) {
+            diceRoll.push(this.numberGenerator())
+        }
+        const lowRoll = Math.min(...diceRoll)
+        return diceRoll.filter(roll => roll !== lowRoll)
+    }
+
+    static generateAbilityScore() {
+        let topThree = this.topthreeRolls()
+            .reduce((runningTotal: number, nextNumber: number) => runningTotal + nextNumber, 0)
+        return topThree
+    }
+
+
+
+
+}
